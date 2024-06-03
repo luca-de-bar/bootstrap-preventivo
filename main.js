a9b690c44a9b61ee823cf233b42a75e43ebae832
luca-de-bar <lucadebar33@gmail.com>
2024-06-03 21:48:44 +0200

    improved: code readability, added descriptive comments.

--- main.js
+++ main.js
@@ -1,49 +1,48 @@
 'use strict';
 
-//Retrieve elements id
+//Form elements id
 const form = document.getElementById('form');
 const submitButton = document.getElementById('submitButton')
 const selectForm = document.getElementById('selectForm');
 const promoInput = document.getElementById('promoInput');
 
-//Modal for valid promo feedback or wrong one.
+//Forms Input to validate
+const nameInput = document.getElementById('nameInput');
+const surnameInput = document.getElementById('surnameInput');
+const emailInput = document.getElementById('emailInput');
+
+//Modals for visual feedback (valid and wrong)
 const wrongPromo = new bootstrap.Modal(document.getElementById("wrongPromo"));
 const validModal = new bootstrap.Modal(document.getElementById("validModal"));
-
-//Valid modal title and p
 const validModalTitle = document.getElementById('validModalTitle');
 const validModalText = document.getElementById('validModalText');
 
-//Prevent Default Form
+
+//submitButton event listener
 form.addEventListener('submit', function (ev) {
+
+  //Ev Prevent Default
   ev.preventDefault()
-})
 
-//Submit event listener
-form.addEventListener('submit', function () {
+  //Check current select element value, calculate price based on it
   const selectFormValue = Number(selectForm.value);
-
   let basePrice;
   switch (selectFormValue) {
     case 1:
       basePrice = 20.50 * 10;
-      console.log(basePrice)
       break;
     case 2:
       basePrice = 15.30 * 10;
-      console.log(basePrice)
       break;
     case 3:
       basePrice = 33.60 * 10;
-      console.log(basePrice)
       break;
   }
 
-  //Check PromoCode Valid
   function isPromoValid(promocodeInserted) {
     const promoAvailable = ['YHDNU32', 'JANJC63', 'PWKCN25', 'SJDPO96', 'POCIE24'];
 
-    //If input not empty, then check value
+    //If Promocode input not empty, then check value in prev. array, return modal
     if (promocodeInserted.trim() != "") {
       if (promoAvailable.includes(promocodeInserted)) {
         validModalTitle.innerHTML = `Sconto applicato!`
@@ -57,6 +56,7 @@ form.addEventListener('submit', function () {
     }
   }
 
+  //If promocode inserted call isPromoValid(), otherwise show validModal
   if (promoInput.value.trim() === "") {
     validModalTitle.innerHTML = `Preventivo Inoltrato`;
     validModalText.innerHTML = `Il costo del progetto è di ${basePrice}€`;
@@ -65,5 +65,3 @@ form.addEventListener('submit', function () {
     isPromoValid(promoInput.value);
   }
 })
-
-//Ho volutamente evitato di validare tramite JS gli altri input.. l'ho già fatto abbastanza negli esercizi precedenti :)
